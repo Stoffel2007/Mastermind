6a2418191fd45aeb4cd4df1e94f677658beca8d0
Stoffel <stoffel.zink@t-online.de>
2016-01-26 08:34:06 +0100

    Added initial files

---
+++ Mastermind/Mastermind/Mastermind/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Mastermind
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
