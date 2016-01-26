8990cfbfeee7350e023755cbfae61aaaf5f38de2
Stoffel <stoffel.zink@t-online.de>
2016-01-26 10:44:56 +0100

    added the small grids to the big grids

--- Mastermind/Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/Mastermind/MainWindow.xaml.cs
@@ -16,16 +16,10 @@ namespace Mastermind
         private void drawGameboards()
         {
             int size = 40;
-
-            gameboard_mastermind.Children.Clear();
-            gameboard_mastermind.RowDefinitions.Clear();
-            gameboard_mastermind.ColumnDefinitions.Clear();
+            
             gameboard_mastermind.Height = 13 * size;
             gameboard_mastermind.Width = 5 * size;
-
-            gameboard_super_mastermind.Children.Clear();
-            gameboard_super_mastermind.RowDefinitions.Clear();
-            gameboard_super_mastermind.ColumnDefinitions.Clear();
+            
             gameboard_super_mastermind.Height = 13 * size;
             gameboard_super_mastermind.Width = 6 * size;
 
@@ -40,6 +34,7 @@ namespace Mastermind
                 gameboard_super_mastermind.RowDefinitions.Add(newRow2);
             }
 
+            // Mastermind
             for (int i = 0; i < 5; i++)
             {
                 ColumnDefinition newCol = new ColumnDefinition();
@@ -47,11 +42,63 @@ namespace Mastermind
                 gameboard_mastermind.ColumnDefinitions.Add(newCol);
             }
 
+            // Super Mastermind
             for (int i = 0; i < 6; i++)
             {
                 ColumnDefinition newCol = new ColumnDefinition();
                 newCol.Width = new GridLength(size);
                 gameboard_super_mastermind.ColumnDefinitions.Add(newCol);
+            }
+
+            for (int i = 1; i < 13; i++)
+            {
+                // Mastermind
+                Grid newGrid1 = new Grid();
+                newGrid1.ShowGridLines = true;
+
+                RowDefinition row1_1 = new RowDefinition();
+                row1_1.Height = new GridLength(size / 2);
+                newGrid1.RowDefinitions.Add(row1_1);
+
+                RowDefinition row1_2 = new RowDefinition();
+                row1_2.Height = new GridLength(size / 2);
+                newGrid1.RowDefinitions.Add(row1_2);
+
+                ColumnDefinition col1_1 = new ColumnDefinition();
+                col1_1.Width = new GridLength(size / 2);
+                newGrid1.ColumnDefinitions.Add(col1_1);
+
+                ColumnDefinition col1_2 = new ColumnDefinition();
+                col1_2.Width = new GridLength(size / 2);
+                newGrid1.ColumnDefinitions.Add(col1_2);
+
+                Grid.SetRow(newGrid1, i);
+                Grid.SetColumn(newGrid1, 0);
+                gameboard_mastermind.Children.Add(newGrid1);
+
+                // Super Mastermind
+                Grid newGrid2 = new Grid();
+                newGrid2.ShowGridLines = true;
+
+                RowDefinition row2_1 = new RowDefinition();
+                row2_1.Height = new GridLength(size / 2);
+                newGrid2.RowDefinitions.Add(row2_1);
+
+                RowDefinition row2_2 = new RowDefinition();
+                row2_2.Height = new GridLength(size / 2);
+                newGrid2.RowDefinitions.Add(row2_2);
+
+                ColumnDefinition col2_1 = new ColumnDefinition();
+                col2_1.Width = new GridLength(size / 2);
+                newGrid2.ColumnDefinitions.Add(col2_1);
+
+                ColumnDefinition col2_2 = new ColumnDefinition();
+                col2_2.Width = new GridLength(size / 2);
+                newGrid2.ColumnDefinitions.Add(col2_2);
+
+                Grid.SetRow(newGrid2, i);
+                Grid.SetColumn(newGrid2, 0);
+                gameboard_super_mastermind.Children.Add(newGrid2);
             }
         }
 
